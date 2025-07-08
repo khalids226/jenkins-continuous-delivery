6e466382c3165481cd17238d910297faea653376
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2025-07-08 14:32:37 +0200

    Update pipeline_07_read_json.groovy

--- pipeline/decrative-pipeline/pipeline_07_read_json.groovy
+++ pipeline/decrative-pipeline/pipeline_07_read_json.groovy
@@ -12,7 +12,7 @@ pipeline {
         stage('get current date') {
             steps {
                 script{
-                    sh 'curl -f -o date.json "http://date.jsontest.com"'
+                    sh 'curl -f -o date.json https://worldtimeapi.org/api/timezone/Etc/UTC'
                     json = readJSON file: 'date.json'
                     echo "TODAY is ${json.date}"
                 }
